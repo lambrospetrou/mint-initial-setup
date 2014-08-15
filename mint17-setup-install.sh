784818a18c0bf491253afce0a2535bec248deb55
Lambros Petrou <lambrospetrou@gmail.com>
2014-08-15 01:22:00 +0100

    some comment editing

--- mint17-setup-install.sh
+++ mint17-setup-install.sh
@@ -12,7 +12,7 @@ apt-get update
 
 apt-get install ubuntu-restricted-extras
 
-# allows to play encrypted DVDs
+# allows to play encrypted DVDs and installs some utilities
 apt-get install libdvdread4 vim htop gparted hardinfo
 
 # install compressing tools
@@ -22,21 +22,14 @@ apt-get install unace p7zip-rar sharutils rar arj lunzip lzip
 apt-get install python python3 build-essential guake git
 
 # install Synapse launcher
-#add-apt-repository ppa:synapse-core/ppa
-#apt-get update
 AddPPA synapse-core/ppa
 apt-get install synapse
 
 # install Sublime Text 2
-#add-apt-repository ppa:webupd8team/sublime-text-2
-#apt-get update
-#apt-get --purge remove sublime-text*
 AddPPA webupd8team/sublime-text-2
 apt-get install sublime-text
 
 # install webupd8 repository
-#add-apt-repository ppa:nilarimogard/webupd8
-#apt-get update
 AddPPA nilarimogard/webupd8
 apt-get install youtube-dl
 
